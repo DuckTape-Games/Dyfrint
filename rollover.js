a58e2e4f0d28eb788cae896e0248bbee4db6e3b9
Chris Herriman <chrisherrimanjr@gmail.com>
2026-04-03 02:00:14 -0400

    Add files via upload

---
+++ rollover.js
@@ -0,0 +1,6 @@
+function MouseRollover(MyImage) {
+    MyImage.src = "Images/products/hoodie/rotatingHoodie.gif";
+}
+function MouseOut(MyImage) {
+    MyImage.src = "Images/products/hoodie/hoodiePrice.png";
+}
